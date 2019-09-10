11559174ae36b119058cee431a981e42a050c17d
Андрей Шпилевой <dron4ik89@gmail.com>
2019-09-10 12:40:18 +0300

    First commit

---
+++ app/src/main/java/andrey/shpilevoy/sample/MainActivity.kt
@@ -0,0 +1,16 @@
+package andrey.shpilevoy.sample
+
+import andrey.shpilevoy.preferences.Preferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Preferences.load("", 1)
+
+    }
+}
---
+++ preferences/src/main/java/andrey/shpilevoy/preferences/Preferences.kt
@@ -0,0 +1,78 @@
+package andrey.shpilevoy.preferences
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class Preferences { // Класс для хранения данных на устройстве
+
+    companion object {
+        //@JvmStatic
+        private var pref: SharedPreferences? = null
+
+        fun init(context : Context){
+            pref = context.getSharedPreferences(context.packageName, 0)
+        }
+
+        fun save(key: String, value: Boolean) {
+            if(pref != null)
+                pref!!.edit().putBoolean(key, value).apply()
+        }
+
+        fun load(key: String, defaultValue: Boolean): Boolean {
+            return if(pref != null) pref!!.getBoolean(key, defaultValue)
+            else defaultValue
+        }
+
+        fun save(key: String, value: Int) {
+            if(pref != null)
+                pref!!.edit().putInt(key, value).apply()
+        }
+
+        fun load(key: String, defaultValue: Int): Int {
+            return if(pref != null) pref!!.getInt(key, defaultValue)
+            else defaultValue
+        }
+
+        fun save(key: String, value: Long) {
+            if(pref != null)
+                pref!!.edit().putLong(key, value).apply()
+        }
+
+        fun load(key: String, defaultValue: Long): Long {
+            return if(pref != null) pref!!.getLong(key, defaultValue)
+            else defaultValue
+        }
+
+        fun save(key: String, value: Float) {
+            if(pref != null)
+                pref!!.edit().putFloat(key, value).apply()
+        }
+
+        fun load(key: String, defaultValue: Float): Float {
+            return if(pref != null) pref!!.getFloat(key, defaultValue)
+            else defaultValue
+        }
+
+        fun save(key: String, value: String) {
+            if(pref != null)
+                pref!!.edit().putString(key, value).apply()
+        }
+
+        fun load(key: String, defaultValue: String): String {
+            return (if(pref != null) pref?.getString(key, defaultValue)
+            else defaultValue) as String
+        }
+
+        fun save(key: String, value: Set<String>) {
+            if(pref != null)
+                pref!!.edit().putStringSet(key, value).apply()
+        }
+
+        fun load(key: String, defaultValue: Set<String>): Set<String> {
+            return (if(pref != null) pref?.getStringSet(key, defaultValue)
+            else defaultValue) as Set<String>
+        }
+
+    }
+
+}
